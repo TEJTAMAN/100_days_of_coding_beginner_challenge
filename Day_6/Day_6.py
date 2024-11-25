5684b84e51ee4edf5f5ab33cdb2786da911e014c
TEJTAMAN <rtejtaman@gmail.com>
2024-11-25 22:54:03 +0530

    Day 6 Uploaded

--- Day_6/Day_6.py
+++ Day_6/Day_6.py
@@ -1,3 +1,6 @@
+#Write a program that takes radius of the circle and outputs the area of the circle to 2 decimal digits
+
+
 radius = float(input("Enter the radius of the circle: "))
 
 if radius < 0:
