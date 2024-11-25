f8cd80cbafd6ca8efaf262296092ed4e663e7aa1
TEJTAMAN <rtejtaman@gmail.com>
2024-11-25 16:33:53 +0530

    Day 8 Uploaded

---
+++ Day_8/Day_8.py
@@ -0,0 +1,41 @@
+def calculator():
+    print("Simple Calculator")
+    print("Select a arithmetic operation: ")
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+
+    arithmetic_operation = input("Enter the choice: ")
+    if arithmetic_operation not in [ '+' , '-' , '*' , '/']:
+        print("Please enter a valid operaation")
+        return
+
+    try:
+        num_1 = int(input("Enter the first integer: "))
+        num_2 = int(input("Enter the second integer: "))
+    except ValueError:
+        print("Invalid input. Please enter integers only")
+        return
+
+    if arithmetic_operation == "+":
+        result = num_1 + num_2
+        operation = "+"
+    elif arithmetic_operation == "-":
+        result = num_1 - num_2
+        operation = "-"
+    elif arithmetic_operation == "*":
+        result = num_1 * num_2
+        operation = "*"
+    elif arithmetic_operation == "/":
+        if num_2 == 0:
+            print("Error: Division by zero is not valid")
+            return
+        result = num_1 / num_2
+        operation = "/"
+
+    print(f"The result of {num_1} {operation} {num_2} is: {result}")
+
+calculator()
+
+    
