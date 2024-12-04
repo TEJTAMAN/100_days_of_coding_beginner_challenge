9bf02edf552e333728a8fa6a5d5a3d5cdae99074
TEJTAMAN <rtejtaman@gmail.com>
2024-12-04 21:18:13 +0530

    Day 16 Uploaded

--- Day_16/Day_16.py
+++ Day_16/Day_16.py
@@ -1,15 +1,13 @@
 #Write a function to check if a given string is a palindrome.
 
 def is_palindrome(s):
-    # Remove non-alphanumeric characters and convert to lowercase
+    
     cleaned = ''.join(char.lower() for char in s if char.isalnum())
-    # Check if the cleaned string is the same when reversed
+   
     return cleaned == cleaned[::-1]
 
-# Input a string
 input_string = input("Enter a string: ")
 
-# Check if the string is a palindrome
 if is_palindrome(input_string):
     print(f'"{input_string}" is a palindrome.')
 else:
