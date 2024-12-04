9171277fff3a614e0c1143324d0a87487597eae2
TEJTAMAN <rtejtaman@gmail.com>
2024-12-04 21:17:54 +0530

    Day 14 Uploaded

--- Day_14/Day_14.py
+++ Day_14/Day_14.py
@@ -7,10 +7,8 @@ def is_leap_year(year):
     else:
         return False
 
-# Input from the user
 year = int(input("Enter a year: "))
 
-# Check and display if the year is a leap year
 if is_leap_year(year):
     print(f"{year} is a leap year.")
 else:
