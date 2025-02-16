12fac5cd033c59641b8c68e0df6166dac831285c
TEJTAMAN <rtejtaman@gmail.com>
2025-02-16 21:53:30 +0530

    Day 60 Uploaded

---
+++ Day_60/Day_60.py
@@ -0,0 +1,10 @@
+#Solve the Tower of Hanoi problem.
+
+
+def tower_hanoi(n, a, b, c):
+    if n == 1:
+        print("move 1st disk from ", a, " to ", c )
+        return
+    tower_hanoi(n-1,a,c,b)
+    print("move", n, "th disk from ", a," to ", c)
+    tower_hanoi(n-1, b, a, c)
